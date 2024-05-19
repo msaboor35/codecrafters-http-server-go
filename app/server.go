c1a06c86fa56c04a719ba97f727b78c25a9109b3
Muhammad Saboor <msaboor35@gmail.com>
2024-05-20 04:17:53 +0500

    feat: accept multiple concurrent requests

--- app/server.go
+++ app/server.go
@@ -12,65 +12,43 @@ const (
 	HTTPVersion = "HTTP/1.1"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-
-	c, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer c.Close()
-
+func processRequest(c net.Conn) error {
 	req, err := parseRequest(c)
 	if err != nil {
 		if errors.Is(err, ErrInvalidRequestLine) {
-			err = NewResponse(404).Write(c)
+			err := NewResponse(404).Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 		} else if errors.Is(err, ErrInvalidMethod) {
-			err = NewResponse(501).Write(c)
+			err := NewResponse(501).Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 		} else if errors.Is(err, ErrInvalidHTTPVersion) {
-			err = NewResponse(505).Write(c)
+			err := NewResponse(505).Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
-			}
-		} else if errors.Is(err, ErrMissingHostHeader) {
-			err = NewResponse(404).Write(c)
-			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 		} else if errors.Is(err, ErrMultipleHostHeader) {
-			err = NewResponse(404).Write(c)
+			err := NewResponse(404).Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 		} else if errors.Is(err, ErrInvalidRequestTarget) {
-			err = NewResponse(404).Write(c)
+			err := NewResponse(404).Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 		}
 
-		fmt.Println("Error parsing request: ", err.Error())
-		os.Exit(1)
+		fmt.Printf("Error parsing request: %v\n", err)
+		return err
 	}
 
 	fmt.Printf("Request: %+v\n", req)
@@ -79,11 +57,11 @@ func main() {
 		if req.RequestTarget == "/" {
 			err = NewResponse(200).Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 
-			return
+			return nil
 		}
 
 		if strings.HasPrefix(req.RequestTarget, "/echo/") {
@@ -94,11 +72,11 @@ func main() {
 				SetBody([]byte(str)).
 				Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 
-			return
+			return nil
 		}
 
 		if req.RequestTarget == "/user-agent" {
@@ -114,24 +92,54 @@ func main() {
 				SetBody([]byte(userAgent)).
 				Write(c)
 			if err != nil {
-				fmt.Println("Error writing response: ", err.Error())
-				os.Exit(1)
+				fmt.Printf("Error writing response: %v\n", err)
+				return err
 			}
 
-			return
+			return nil
 		}
 
 		err = NewResponse(404).Write(c)
 		if err != nil {
-			fmt.Println("Error writing response: ", err.Error())
-			os.Exit(1)
+			fmt.Printf("Error writing response: %v\n", err)
+			return err
 		}
 	} else {
 		err = NewResponse(404).Write(c)
 		if err != nil {
-			fmt.Println("Error writing response: ", err.Error())
+			fmt.Printf("Error writing response: %v\n", err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func handleConnection(c net.Conn) {
+	err := processRequest(c)
+	if err != nil {
+		fmt.Println("Error processing request: ", err.Error())
+		return
+	}
+}
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
+		os.Exit(1)
+	}
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
+		go handleConnection(c)
 	}
 
 }
